c3f637dc8de2fb10dd0cf0780a2ca95cc0ee2218
JouskaPram <pramudita5800@gmail.com>
2022-12-07 11:07:00 +0700

    add delete and auth fitur

--- perpuspram/urls.py
+++ perpuspram/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from perpustakaan.views import *
-
-
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',main),
@@ -10,7 +10,8 @@ urlpatterns = [
     path('buku/', buku),
     path('penerbit/',penerbit),
     path('tambah/',tambah_buku),
-    path('buku/ubah/<int:id_buku>',ubah_buku,name="ubah_buku")
-    
+    path('buku/ubah/<int:id_buku>',ubah_buku,name="ubah_buku"),
+    path('buku/delete/<int:id_buku>',delete_buku,name="delete_buku"),
+    path('login/',LoginView.as_view(),name="login"),
 ]
  
--- perpustakaan/views.py
+++ perpustakaan/views.py
@@ -2,21 +2,31 @@ from django.shortcuts import render, redirect
 from perpustakaan.models import Buku
 from .forms import FormBuku
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+# delete
+@login_required(login_url='login')
+def delete_buku(request,id_buku):
+    buku = Buku.objects.filter(id=id_buku)
+    buku.delete()
 
+    return redirect('/buku/')
 
-
+@login_required(login_url='login')
 def buku(request):
     books = Buku.objects.all()
     konteks={
        'books': books,
     }
     return render(request,'buku.html',konteks)
+@login_required(login_url='login')
 def penerbit(request):
     return render(request,'penerbit.html')
-
+@login_required(login_url='login')
 def main(request):
     return render(request,'main.html')
 
+@login_required(login_url='login')
 def tambah_buku(request):
     if request.POST:
         form = FormBuku(request.POST)
@@ -37,6 +47,7 @@ def tambah_buku(request):
           'form':form,
         }
         return render(request,'tambah-buku.html',konteks)
+@login_required(login_url='login')       
 def ubah_buku(request,id_buku):
     buku = Buku.objects.get(id=id_buku)
     template = "ubah-buku.html"
@@ -55,3 +66,5 @@ def ubah_buku(request,id_buku):
         }
 
     return render(request,template,konteks)
+
+
